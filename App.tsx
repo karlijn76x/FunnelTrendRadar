b8ed9b2b27d7fef743bae8f0c86de648d36d76b6
elitsa1819 <elitsa1819@gmail.com>
2025-05-16 12:51:52 +0200

    adding logo in the app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={styles.tinyLogo}
+        source={require('./assets/vanderlande_logo.png')}
+      />
     </View>
   );
 }
 
+//Styling of logo
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  tinyLogo: {
+    width: 400,
+    height: 60,
   },
 });
