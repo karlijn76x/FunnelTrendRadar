3fe34f58fd00c9f7f788e40da98d3b3c40644b33
Karlijn vd Herik <123644684+karlijn76x@users.noreply.github.com>
2025-05-20 11:06:21 +0200

    Trends

--- App.tsx
+++ App.tsx
@@ -4,12 +4,11 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import TrendDetail from './Components/TrendDetail';
 
 export default function App() {
-    const [visible, setVisible] = useState(false);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-            <Button title="Toon Popup" onPress={() => setVisible(true)} />
-      <TrendDetail visible={visible} onClose={() => setVisible(false)} />
+
       <StatusBar style="auto" />
     </View>
   );
