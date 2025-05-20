913a2bac481b55af2f7bff5ac5f824fc23e40c11
Luuk <luuksteusfij@gmail.com>
2025-05-20 10:44:24 +0200

    Fixed merge

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
-
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Dropdown } from 'react-native-element-dropdown';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import DropdownComponent from './components/Dropdown_menu';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import DropdownComponent from './Components/Dropdown_menu';
 import { useFonts } from 'expo-font';
 
 
@@ -16,61 +14,60 @@ export default function App() {
   });
 
   return (
-    <View style={styles.container}>
+    <View>
       <Image
         style={styles.tinyLogo}
         source={require('./assets/vanderlande_logo.png')}
       />
       <View style = {{backgroundColor:'#FFEFDF', margin:20, padding:20, borderColor:'black', borderRadius:20, borderWidth: 2, alignItems: 'center', justifyContent: 'center'}}>
-      <DropdownComponent />
-
-      <View style={{flexDirection: 'row', alignItems: 'center'}}>
-        <View style={styles.column}>
-          <Text style={{ fontFamily: 'Aptos_ExtraBold' }}>5-10 years</Text>
-        </View>
-        <Image
-            style={{ width: 450, height: 50, resizeMode: 'cover' }}
-            source={require('./assets/images/funnel_top.png')}
-        />
+        <DropdownComponent />
       </View>
-      <View style={{flexDirection: 'row', alignItems: 'center'}}>
-        <View style={styles.column}>
-          <Text style={{ fontFamily: 'Aptos_ExtraBold' }}>3-5 years</Text>
-        </View>
-        <Image
-            style={{ width: 400, height: 50, resizeMode: 'cover' }}
-            source={require('./assets/images/funnel_middle.png')}
-        />
-      </View>
-      <View style={{flexDirection: 'row'}}>
-        <View style={[styles.column, {paddingTop: 15}]}>
-          <Text style={{ fontFamily: 'Aptos_ExtraBold' }}>0-3 years</Text>
-        </View>
-        <Image
-            style={{ width: 350, height: 125, resizeMode: 'cover' }}
-            source={require('./assets/images/funnel_bottom.png')}
-        />
-
+      <View style={styles.container}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <View style={styles.column}>
+              <Text style={{ fontFamily: 'Aptos_ExtraBold' }}>5-10 years</Text>
+            </View>
+            <Image
+                style={{ width: 450, height: 50, resizeMode: 'cover' }}
+                source={require('./assets/images/funnel_top.png')}
+            />
+          </View>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <View style={styles.column}>
+              <Text style={{ fontFamily: 'Aptos_ExtraBold' }}>3-5 years</Text>
+            </View>
+            <Image
+                style={{ width: 400, height: 50, resizeMode: 'cover' }}
+                source={require('./assets/images/funnel_middle.png')}
+            />
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <View style={[styles.column, {paddingTop: 15}]}>
+              <Text style={{ fontFamily: 'Aptos_ExtraBold' }}>0-3 years</Text>
+            </View>
+            <Image
+                style={{ width: 350, height: 125, resizeMode: 'cover' }}
+                source={require('./assets/images/funnel_bottom.png')}
+            />
+         </View>
       </View>
     </View>
   );
 }
 
-//Styling of logo
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: 3,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   tinyLogo: {
     marginLeft:30,
     marginTop:50,
     width: 400,
     height: 60,
--------
-    gap: 3,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   column: {
     paddingRight: 25,
@@ -78,4 +75,3 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end'
   }
 });
-
