b758e55cefce5a16d8a8034861302f7e22826a04
Jasmin Hachmane <509304@student.fontys.nl>
2025-05-22 17:54:20 +0200

    Added 'very high' impact to SocialTrend and TechTrend components + displayed it in the funnel.

--- components/TechTrendCircle.tsx
+++ components/TechTrendCircle.tsx
@@ -1,47 +1,48 @@
 import React from 'react';
 import { View, StyleSheet, Pressable, Text } from 'react-native';
 
-type Impact = 'low' | 'medium' | 'high';
+type Impact = 'low' | 'medium' | 'high' | 'veryHigh'; 
 
 type Props = {
   impact: Impact;
   onPress: () => void;
-  style?: any;  // style prop toevoegen zodat je externe styles kunt doorgeven
+  style?: any;
 };
 
 const getSize = (impact: Impact): number => {
   switch (impact) {
     case 'low':
-      return 40;
+      return 35;
     case 'medium':
-      return 60;
+      return 55;
     case 'high':
-      return 80;
+      return 75;
+    case 'veryHigh':
+      return 90;
     default:
       return 50;
   }
 };
 
 const TechTrendCircle: React.FC<Props> = ({ impact, onPress, style }) => {
-    const size = getSize(impact);
-  
-    return (
-      <Pressable onPress={onPress} style={[{ zIndex: 1 }, style]}>
-        <View
-          style={[
-            styles.circle,
-            {
-              width: size,
-              height: size,
-              borderRadius: size / 2,
-            },
-          ]}
-        >
-        </View>
-      </Pressable>
-    );
-  };
-  
+  const size = getSize(impact);
+
+  return (
+    <Pressable onPress={onPress} style={[{ zIndex: 1 }, style]}>
+      <View
+        style={[
+          styles.circle,
+          {
+            width: size,
+            height: size,
+            borderRadius: size / 2,
+          },
+        ]}
+      >
+      </View>
+    </Pressable>
+  );
+};
 
 const styles = StyleSheet.create({
   circle: {
