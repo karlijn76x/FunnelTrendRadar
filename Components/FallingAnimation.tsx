d5dc45a3610042a6b0989b057d847d1f9d17657a
briahnaaztria19 <briahnaaztria19@gmail.com>
2025-06-10 16:40:02 +0200

    added basic animation

--- Components/FallingAnimation.tsx
+++ Components/FallingAnimation.tsx
@@ -1,14 +1,45 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useRef } from 'react';
+import { Animated, StyleSheet } from 'react-native';
 
-const FallingAnimation = () => {
-  return (
-    <View>
-      <Text>FallingAnimation</Text>
-    </View>
-  )
+interface FallingAnimationProps {
+  targetX: number;
+  targetY: number;
+  delay: number;
+  children: React.ReactNode;
 }
 
-export default FallingAnimation
+const FallingAnimation: React.FC<FallingAnimationProps> = ({ 
+  targetX, 
+  targetY, 
+  delay, 
+  children,
+}) => {
+  const position = useRef(new Animated.ValueXY()).current;
 
-const styles = StyleSheet.create({})
+  useEffect(() => {
+    position.setValue({ x: targetX - 100, y: -100 });
+    
+    Animated.sequence([
+      Animated.delay(delay),
+      Animated.timing(position, {
+        toValue: { x: targetX, y: targetY },
+        duration: 2000,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
+
+  return (
+    <Animated.View style={[styles.container, position.getLayout()]}>
+      {children}
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+  },
+});
+
+export default FallingAnimation;
