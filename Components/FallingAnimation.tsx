c43045cf3022e8843cab4808dd5223169bac2a2f
briahnaaztria19 <briahnaaztria19@gmail.com>
2025-06-10 16:00:18 +0200

    created FallingAnimation component

---
+++ Components/FallingAnimation.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const FallingAnimation = () => {
+  return (
+    <View>
+      <Text>FallingAnimation</Text>
+    </View>
+  )
+}
+
+export default FallingAnimation
+
+const styles = StyleSheet.create({})
