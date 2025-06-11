dd353fb6614174937742c1b8071682c53241a811
briahnaaztria19 <briahnaaztria19@gmail.com>
2025-06-11 22:20:45 +0200

    created CreateTrend page

---
+++ pages/CreateTrend.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CreateTrend = () => {
+  return (
+    <View>
+      <Text>CreateTrend</Text>
+    </View>
+  )
+}
+
+export default CreateTrend
+
+const styles = StyleSheet.create({})
