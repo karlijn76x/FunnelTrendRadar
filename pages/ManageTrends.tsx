18eff9ee4ca527f55060cf5a47847f0cc49a435d
elitsa1819 <elitsa1819@gmail.com>
2025-06-12 11:54:52 +0200

    adding styling and trying to add a table

--- pages/ManageTrends.tsx
+++ pages/ManageTrends.tsx
@@ -2,16 +2,20 @@ import React, { useState } from 'react'
 import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
 import NavBarEdit from '../components/NavBarEditTrends';
 import SearchBar from '../components/Search_bar';
+import ManagementTable from "../components/ManagementTable"
 
 function ManageTrends() {
 
   return (
     <View style={styles.container}>
       <NavBarEdit/>
-      <View>
-        <Text>Manage Trends</Text>
-        <SearchBar />
+
+      <View style={styles.titleAndSearch}>
+        <Text style={styles.title}>Manage Trends</Text>
+        <SearchBar/>
       </View>
+
+      <ManagementTable/>
     </View>
   )
 }
@@ -22,5 +26,17 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     flex: 1,
+  },
+  titleAndSearch:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    marginVertical:40,
+    marginHorizontal:60,
+  },
+  title:{
+    fontFamily:'Aptos',
+    fontSize:50,
+    fontWeight:'bold',
   },
 })
